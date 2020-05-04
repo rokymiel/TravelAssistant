80a8bd00d1df3c17060d4bd488e2d85d4cf7b728
rokymiel <rokymiel@gmail.com>
2020-05-04 22:26:04 +0300

    Комментарии(снова)

--- TravelAssistant/Managers/FileAccessManager.cs
+++ TravelAssistant/Managers/FileAccessManager.cs
@@ -5,6 +5,9 @@ namespace TravelAssistant.Managers
 {
     public class FileAccessManager
     {
+        /// <summary>
+        /// Получение локального пути к файлам.
+        /// </summary>
         public static string GetLocalFilePath(string filename)
         {
             return System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
