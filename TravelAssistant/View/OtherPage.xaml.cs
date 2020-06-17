b4e31d723ca798d872b8d6f109b6db582612506c
rokymiel <rokymiel@gmail.com>
2020-06-17 23:42:31 +0300

    Оставшиеся фалы OtherPage

--- TravelAssistant/View/OtherPage.xaml.cs
+++ TravelAssistant/View/OtherPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using TravelAssistant.Managers;
 using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
 
 namespace TravelAssistant.View
 {
@@ -14,16 +15,24 @@ namespace TravelAssistant.View
 
         async void OnFinanceTabbed(System.Object sender, System.EventArgs e)
         {
+            (sender as PancakeView).IsEnabled = false;
+            await AnimationManager.StartScalePancakeView(sender as PancakeView);
             await Navigation.PushAsync(new FinancePage());
+            (sender as PancakeView).IsEnabled = true;
         }
 
         async void OnTrip_Tapped(System.Object sender, System.EventArgs e)
         {
+            (sender as PancakeView).IsEnabled = false;
+            await AnimationManager.StartScalePancakeView(sender as PancakeView);
             await Navigation.PushAsync(new TripDetailsPage());
+            (sender as PancakeView).IsEnabled = true;
         }
 
-        void Exit_Tapped(System.Object sender, System.EventArgs e)
+        async void Exit_Tapped(System.Object sender, System.EventArgs e)
         {
+            (sender as PancakeView).IsEnabled = false;
+            await AnimationManager.StartScalePancakeView(sender as PancakeView);
             if (App.Current.Properties.ContainsKey("mainPage"))
                 App.Current.Properties.Remove("mainPage");
             App.Current.MainPage = new NavigationPage(new AllTripsPage());
@@ -31,7 +40,10 @@ namespace TravelAssistant.View
 
         async void AboutApp_Tapped(System.Object sender, System.EventArgs e)
         {
+            (sender as PancakeView).IsEnabled = false;
+            await AnimationManager.StartScalePancakeView(sender as PancakeView);
             await Navigation.PushAsync(new AboutAppPage());
+            (sender as PancakeView).IsEnabled = true;
         }
     }
 }
