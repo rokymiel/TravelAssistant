360fb4f02463068240d2ccf495da3206b87b4b1f
rokymiel <rokymiel@gmail.com>
2020-06-17 23:43:20 +0300

    Изменено взаимодействие с выезжающими страницами

--- TravelAssistant/View/FinanceInfoPage.xaml.cs
+++ TravelAssistant/View/FinanceInfoPage.xaml.cs
@@ -23,6 +23,18 @@ namespace TravelAssistant.View
             spentMoneyLabel.Text = string.Format($"{Money.AllMoney - Money.CurrentMoney:N}");
             Animation = new Rg.Plugins.Popup.Animations.MoveAnimation();
         }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            cakeView.HeightRequest = GetHeight;
+            Console.WriteLine(GetHeight);
+        }
+        protected override void OnAppearingAnimationBegin()
+        {
+            cakeView.HeightRequest = GetHeight;
+            Console.WriteLine(GetHeight);
+            base.OnAppearingAnimationBegin();
+        }
         async void deleteHistory_Clicked(object sender, EventArgs e)
         {
             App.moneyManager.DeleteAll(MainPage.CurrentTrip.Id);
@@ -31,7 +43,40 @@ namespace TravelAssistant.View
             FinancePage.CloseFinancePage();
 
         }
+        double savedTranslationY;
+        double lastTranslationY;
+        double GetHeight => page.Height * 0.8;
+        async void PanGestureRecognizer_PanUpdated(System.Object sender, Xamarin.Forms.PanUpdatedEventArgs e)
+        {
 
+            switch (e.StatusType)
+            {
+
+                case GestureStatus.Running:
+
+                    //Console.WriteLine(e.TotalY);
+                    if (e.TotalY + savedTranslationY > 0)
+                    {
+                        cakeView.TranslationY = e.TotalY + savedTranslationY;
+                        lastTranslationY = cakeView.TranslationY;
+                        //Math.Max(Math.Min(0, cakeView.Y + e.TotalY),-Math.Abs(Content.Height - page.Height));
+                    }
+                    break;
+                case GestureStatus.Completed:
+                    savedTranslationY = lastTranslationY;
+                    if (savedTranslationY <= GetHeight * 0.14)
+                    {
+                        savedTranslationY = cakeView.TranslationY = 0;
+                    }
+                    else
+                    {
+                        await Navigation.PopPopupAsync();
+                    }
+
+                    //Console.WriteLine("Completed");
+                    break;
+            }
+        }
 
     }
 }
--- TravelAssistant/View/OperationDetailsPage.xaml.cs
+++ TravelAssistant/View/OperationDetailsPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using TravelAssistant.Models;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using Rg.Plugins.Popup.Extensions;
 
 namespace TravelAssistant.View
 {
@@ -17,6 +19,51 @@ namespace TravelAssistant.View
             InitializeComponent();
             Animation = new Rg.Plugins.Popup.Animations.MoveAnimation();
             cakeView.BindingContext = Money;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            cakeView.HeightRequest = GetHeight;
+            //Console.WriteLine(GetHeight);
+        }
+        protected override void OnAppearingAnimationBegin()
+        {
+            cakeView.HeightRequest = GetHeight;
+            Console.WriteLine(GetHeight);
+            base.OnAppearingAnimationBegin();
+        }
+        double savedTranslationY;
+        double lastTranslationY;
+        double GetHeight => page.Height * 0.8;
+        async void PanGestureRecognizer_PanUpdated(System.Object sender, Xamarin.Forms.PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+
+                case GestureStatus.Running:
+
+                    //Console.WriteLine(e.TotalY);
+                    if (e.TotalY + savedTranslationY > 0)
+                    {
+                        cakeView.TranslationY = e.TotalY + savedTranslationY;
+                        lastTranslationY = cakeView.TranslationY;
+                        //Math.Max(Math.Min(0, cakeView.Y + e.TotalY),-Math.Abs(Content.Height - page.Height));
+                    }
+                    break;
+                case GestureStatus.Completed:
+                    savedTranslationY = lastTranslationY;
+                    if (savedTranslationY <= GetHeight * 0.14)
+                    {
+                        savedTranslationY = cakeView.TranslationY = 0;
+                    }
+                    else
+                    {
+                        await Navigation.PopPopupAsync();
+                    }
+
+                    //Console.WriteLine("Completed");
+                    break;
+            }
         }
     }
 }
